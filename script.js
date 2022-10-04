1c9f2191d23caff5dc46834eccf9d337fa857ba2
asjadahamad <mailasjadahmad@gmail.com>
2022-10-04 14:26:44 +0530

    script tag fixxed

--- script.js
+++ script.js
@@ -1,50 +1,4 @@
-let submitBtn = document.querySelector(".submit-task");
-submitBtn.addEventListener("click", () => {
-  let singleTaskText = document.getElementById("task-store");
-  let titleText = document.querySelector("h2");
 
-  if (!singleTaskText.value) {
-    titleText.innerText = "Enter something before submit";
-  } else {
-    titleText.innerText = "Create your task";
-    let singleTodo = document.createElement("div");
-    singleTodo.classList.add("single-todo-container");
-    singleTodo.innerHTML = `
-    <input type="text" 
-    value="${singleTaskText.value}"
-    class="task-input"
-    readonly >
-    <button class="edit-single-todo">Edit</button>
-    <button class="delete-single-todo">Delete</button>
-  `;
-
-    let todoListContainer = document.querySelector(".todo-list-container");
-    todoListContainer.appendChild(singleTodo);
-
-    let inputField = singleTodo.querySelector(".task-input");
-    let editBtn = singleTodo.querySelector(".edit-single-todo");
-    let deleteBtn = singleTodo.querySelector(".delete-single-todo");
-
-    editBtn.addEventListener("click", () => {
-      if (editBtn.innerText == "Edit") {
-        inputField.removeAttribute("readonly");
-        inputField.style.color = "grey";
-        editBtn.innerText = "Save";
-      } else if (editBtn.innerText == "Save") {
-        inputField.setAttribute("readonly", "readonly");
-        inputField.style.color = "black";
-        editBtn.innerText = "Edit";
-      }
-    });
-    deleteBtn.addEventListener("click", () => {
-      todoListContainer.removeChild(singleTodo);
-      savetodo();
-    });
-    singleTaskText.value="";
-    savetodo();
-
-  }
-});
 // setInterval( savetodo,2000);
 
 function savetodo() {
@@ -96,5 +50,52 @@ window.addEventListener("load", () => {
         savetodo();
       });
     }
+    let submitBtn = document.querySelector(".submit-task");
+submitBtn.addEventListener("click", () => {
+  let singleTaskText = document.getElementById("task-store");
+  let titleText = document.querySelector("h2");
+
+  if (!singleTaskText.value) {
+    titleText.innerText = "Enter something before submit";
+  } else {
+    titleText.innerText = "Create your task";
+    let singleTodo = document.createElement("div");
+    singleTodo.classList.add("single-todo-container");
+    singleTodo.innerHTML = `
+    <input type="text" 
+    value="${singleTaskText.value}"
+    class="task-input"
+    readonly >
+    <button class="edit-single-todo">Edit</button>
+    <button class="delete-single-todo">Delete</button>
+  `;
+
+    let todoListContainer = document.querySelector(".todo-list-container");
+    todoListContainer.appendChild(singleTodo);
+
+    let inputField = singleTodo.querySelector(".task-input");
+    let editBtn = singleTodo.querySelector(".edit-single-todo");
+    let deleteBtn = singleTodo.querySelector(".delete-single-todo");
+
+    editBtn.addEventListener("click", () => {
+      if (editBtn.innerText == "Edit") {
+        inputField.removeAttribute("readonly");
+        inputField.style.color = "grey";
+        editBtn.innerText = "Save";
+      } else if (editBtn.innerText == "Save") {
+        inputField.setAttribute("readonly", "readonly");
+        inputField.style.color = "black";
+        editBtn.innerText = "Edit";
+      }
+    });
+    deleteBtn.addEventListener("click", () => {
+      todoListContainer.removeChild(singleTodo);
+      savetodo();
+    });
+    singleTaskText.value="";
+    savetodo();
+
+  }
+});
   }
 });
